033d031b96c039b6a1ad81b29a1711fbc29d9e32
Deepak130597 <102418484+Deepak130597@users.noreply.github.com>
2022-04-29 11:16:08 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+ import React from 'react';
+ import Resturent from './Component/Basic/Resturent';
+ 
+ 
+ const App = () => {
+   return (
+     <>
+     <Resturent/>
+     </>
+   )
+ }
+ 
+ export default App
+
